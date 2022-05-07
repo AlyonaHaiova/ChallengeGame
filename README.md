fee84e1daa4b7b9f3d3f5d09d33845d152b9f712
AndriiBosyk <andriybosik2002@ukr.net>
2022-05-08 01:12:26 +0300

    API | Configure docker-compose

---
+++ README.md
@@ -0,0 +1,7 @@
+# Getting Started
+
+## Docker
+
+You can run this project with `docker-compose up` command.
+Note that you need to define environment variables required for application to start.
+Read the documentation: https://docs.docker.com/compose/environment-variables/
--- update-api.sh
+++ update-api.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-image_name=${1:-challenge_game}
+image_name=${1:-challenge_game/api}
 container_name=${2:-challenge_game_api}
 
 git pull origin main
@@ -9,7 +9,4 @@ docker rm -f $container_name
 docker rmi -f $image_name
 docker image rm -f "$(docker images -f dangling=true -q)"
 
-cd ./game-api || exit
-
-docker build -t challenge_game .
-docker run -d -p 8080:8080 --name challenge_game_api challenge_game
+docker-compose up -d
