217df74d19d4ae3b112e9e7a5903943245004bfa
Alyona Haiova <alenag2602@gmail.com>
2022-06-23 15:42:19 +0300

    fix

--- game-api/src/main/java/com/example/gameapi/controller/GameController.java
+++ game-api/src/main/java/com/example/gameapi/controller/GameController.java
@@ -1,6 +1,10 @@
 package com.example.gameapi.controller;
 
-import com.example.gameapi.dto.*;
+import com.example.gameapi.dto.CardDto;
+import com.example.gameapi.dto.CreateGameDto;
+import com.example.gameapi.dto.IdDto;
+import com.example.gameapi.dto.RandomCardDto;
+import com.example.gameapi.dto.RoleDto;
 import com.example.gameapi.meta.Endpoint;
 import com.example.gameapi.meta.Purpose;
 import com.example.gameapi.service.GameService;
@@ -8,7 +12,13 @@ import com.example.gameapi.service.RoleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
