1dd951ce528e40cb1bdaad9ee07c0913523e82fe
Alyona Haiova <54181677+alyona26o2@users.noreply.github.com>
2022-07-11 16:05:48 +0300

    fix validation bug (#17)

--- game-api/src/main/java/com/example/gameapi/annotation/Ranged.java
+++ game-api/src/main/java/com/example/gameapi/annotation/Ranged.java
@@ -3,12 +3,12 @@ package com.example.gameapi.annotation;
 import com.example.gameapi.config.RangedValidator;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -20,4 +20,8 @@ public @interface Ranged {
   String begin();
 
   String end();
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
 }
