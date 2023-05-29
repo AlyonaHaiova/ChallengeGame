94fd86bce0609fc9710e69441b4601b62bd33092
alyona26o2 <alenag2602@gmai.com>
2023-05-29 13:42:46 +0300

    "Fix"

--- game-api/src/main/java/com/example/gameapi/config/CorsConfig.java
+++ game-api/src/main/java/com/example/gameapi/config/CorsConfig.java
@@ -1,20 +1,18 @@
-package com.example.gameapi.config;
+ package com.example.gameapi.config;
+ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+ import org.springframework.web.servlet.config.annotation.CorsRegistry;
+ import org.springframework.context.annotation.Configuration;
+ @Configuration
+ @EnableWebMvc
+ public class CorsConfig implements WebMvcConfigurer {
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
+   @Override
+   public void addCorsMappings(CorsRegistry registry) {
+     registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000")
         .allowedMethods("*")
         .allowedHeaders("*")
         .allowCredentials(true);
-  }
-}
+   }
+ }
--- game-api/src/main/java/com/example/gameapi/controller/CardController.java
+++ game-api/src/main/java/com/example/gameapi/controller/CardController.java
@@ -1,41 +1,38 @@
-package com.example.gameapi.controller;
-
-import com.example.gameapi.dto.CardDto;
-import com.example.gameapi.dto.FullCardDataDto;
-import com.example.gameapi.dto.IdDto;
-import com.example.gameapi.dto.RandomCardDto;
-import com.example.gameapi.meta.Endpoint;
-import com.example.gameapi.service.CardService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+ package com.example.gameapi.controller;
+ import com.example.gameapi.dto.CardDto;
+ import com.example.gameapi.dto.FullCardDataDto;
+ import com.example.gameapi.dto.IdDto;
+ import com.example.gameapi.dto.RandomCardDto;
+ import com.example.gameapi.meta.Endpoint;
+ import com.example.gameapi.service.CardService;
+ import lombok.RequiredArgsConstructor;
+ import org.springframework.http.HttpStatus;
+ import org.springframework.http.ResponseEntity;
+ import org.springframework.web.bind.annotation.DeleteMapping;
+ import org.springframework.web.bind.annotation.GetMapping;
+ import org.springframework.web.bind.annotation.PathVariable;
+ import org.springframework.web.bind.annotation.PostMapping;
+ import org.springframework.web.bind.annotation.PutMapping;
+ import org.springframework.web.bind.annotation.RequestBody;
+ import org.springframework.web.bind.annotation.RequestMapping;
+ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import java.util.List;
 
-@RestController
-@RequestMapping(Endpoint.CARD)
-@RequiredArgsConstructor
-public class CardController {
-  private final CardService cardService;
-
-  @PostMapping
-  public ResponseEntity<IdDto> create(
-      @PathVariable("gameId") long gameId,
-      @Valid @RequestBody CardDto cardDto
-  ) {
-    return ResponseEntity
-        .status(HttpStatus.CREATED)
-        .body(cardService.create(cardDto));
-  }
+ @RestController
+ @RequestMapping(Endpoint.CARD)
+ @RequiredArgsConstructor
+ public class CardController {
+   private final CardService cardService;
+    @PostMapping
+    public ResponseEntity<IdDto> create(
+        @PathVariable("gameId") long gameId,
+        @Valid @RequestBody CardDto cardDto
+    ) {
+      return ResponseEntity
+          .status(HttpStatus.CREATED)
+          .body(cardService.create(cardDto));
+    }
 
   @PutMapping("/{id}")
   public void update(
@@ -44,7 +41,7 @@ public class CardController {
       @Valid @RequestBody CardDto cardDto
   ) {
     cardService.update(id, cardDto);
-  }
+   }
 
   @DeleteMapping("/{id}")
   public void delete(
@@ -81,4 +78,4 @@ public class CardController {
   ) {
     return cardService.getRandomPenaltyCard(gameId);
   }
-}
+ }
